828d708b13003c57da8e1cba4eb1585938f762e8
RojaP <9999pranav@gmail.com>
2023-02-28 11:40:22 +0530

    Add files via upload
    arithmetic opeartions class added

---
+++ WebApplication8/ArithmeticOperations.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication8
+{
+    public class ArithmeticOperations
+    {
+        public int Operand1 { get; set; }
+        public int Operand2 { get; set; }
+
+        public int Add()
+        {
+            return Operand1 + Operand2;
+        }
+        public int Sub()
+        {
+            return Operand1 - Operand2;
+        }
+    }
+}
